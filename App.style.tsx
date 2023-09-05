f7b34dc28a90532cbe75e3237315a705241e19f4
Caique <caiquemorales20@gmail.com>
2023-09-05 02:14:11 -0300

    :tada: First commit

---
+++ App.style.tsx
@@ -0,0 +1,14 @@
+// Imports
+import styled from 'styled-components/native';
+
+// Styled Components
+export const ViewS = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background-color: #262626;
+`;
+
+export const TextS = styled.Text`
+  color: #ffffff;
+`;
--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { TextS, ViewS } from './App.style';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <ViewS>
       <StatusBar style="auto" />
-    </View>
+      <TextS>App</TextS>
+    </ViewS>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
